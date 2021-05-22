706149045e93f2dc93ddd4b42791e51e1ba410f8
Scott Cerny <scottcerny@yahoo.com>
2021-05-21 20:34:27 -0700

    created placeholder routes

--- app.py
+++ app.py
@@ -13,14 +13,37 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
-        f"placeholder"
+        f"Welcome to the Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation</br>"
+        f"/api/v1.0/stations</br>"
+        f"/api/v1.0/tobs</br>"
+        f"/api/v1.0/<start></br>"
+        f"/api/v1.0/<start>/<end>"
         )
 
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    return "precip placeholder"
+
+@app.route("/api/v1.0/stations")
+def stations():
+    return "stations placeholder"
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    return "tobs placeholder"
+
+@app.route("/api/v1.0/<start>")
+def temps(start):
+    return "placeholder"
+
+@app.route("/api/v1.0/<start>/<end>")
+def temps2(startend):
+    return "placeholder2"
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
